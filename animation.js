f0068cd0ac1ad7e2a8eecd32e22f38aef7ce1e00
Azzuhry <muhammadazzuhry03@gmail.com>
2021-02-08 19:10:57 +0700

    added anime.js library

---
+++ animation.js
@@ -0,0 +1,8 @@
+anime({
+    targets: '.youtube path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    direction: 'alternate',
+    loop: true
+})

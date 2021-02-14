9e1dd608ee60b2964ca74a37efed5ad24e335b8e
Azzuhry <muhammadazzuhry03@gmail.com>
2021-02-14 17:26:56 +0700

    complete project overhaul

--- animation.js
+++
@@ -1,8 +0,0 @@
-anime({
-    targets: '.youtube path',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    easing: 'easeInOutSine',
-    duration: 1500,
-    direction: 'alternate',
-    loop: true
-})
--- app.js
+++
@@ -1,8 +0,0 @@
-let userName = document.getElementById('username');
-let passWord = document.getElementById('password');
-
-function preventLogin() {
-    if(userName !="", passWord !="") {
-        alert("please fill required form below");
-    } 
-}
